65d67f5092863a729b9b4648697ec3241bde5303
yukisa2010 <43121712+yukisa2010@users.noreply.github.com>
2019-10-30 01:41:29 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,161 @@
+
+class FromValidator {
+  constructor() {
+    this.config = {
+      title: {
+        maxLength: 10,
+        minLength: 3
+      },
+      desc: {
+        maxLength: 20,
+        minLength: 10
+      } 
+    }
+
+    this.$title = document.getElementsByName('title')[0];
+    this.$desc = document.getElementsByName('text-area')[0];
+    this.$btn = document.getElementsByName('btn')[0];
+    this.$errorsField = document.getElementsByClassName('error-msg')[0];
+    this.$btn.disabled = 'true';
+    this.titleStr = this.$title.value
+    this.descStr = this.$desc.value
+    this.errors = [] 
+    this.handleEvent()
+  }
+
+  handleEvent() {
+
+    const self = this
+    this.$title.addEventListener("keyup", function(){
+      self.displayErrors()
+    })
+    this.$desc.addEventListener("keyup", function(){
+      self.displayErrors()
+    })
+  }
+
+  valid() {
+    this.errors = []
+
+    this.titleStr = this.$title.value
+    this.descStr = this.$desc.value
+
+    if (this.titleStr.length < this.config.title.minLength) {
+      this.errors.push(`タイトルは${this.config.title.minLength}以上で入力してください`)
+    } else if (this.titleStr.length > this.config.title.maxLength) {
+      this.errors.push(`タイトルは${this.config.title.maxLength}以下で入力してください`)
+    } 
+    
+    if (this.descStr.length < this.config.desc.minLength) {
+      this.errors.push(`タイトルは${this.config.desc.minLength}以上で入力してください`)
+    } else if(this.descStr.length > this.config.desc.maxLength) {
+      this.errors.push(`タイトルは${this.config.desc.maxLength}以下で入力してください`)
+    }
+
+    return this.errors.length === 0
+  } 
+
+  displayErrors() {
+    const self = this;
+    this.$errorsField.innerHTML = ''
+    if(!this.valid()) {
+      this.errors.forEach(function(element, index) {
+        const li = document.createElement('li')
+        li.textContent = element
+        self.$errorsField.appendChild(li)
+      });
+      this.$btn.disabled = 'true';
+    } else {
+      this.$btn.disabled = '';
+    }
+  }
+} 
+
+new FromValidator()
+// const titleConfig = {
+//   name: 'title',
+//   min: {
+//     size: 3,
+//     message: `タイトルは3文字以上入力してください`
+//   },
+//   max: {
+//     size: 10,
+//     message: `タイトルは10文字以上入力してください`
+//   }
+// }
+
+// const textAreaConfig = {
+//   name: 'text-area',
+//   min: {
+//     size: 10,
+//     message: `本文は10文字以上入力してください`
+//   },
+//   max: {
+//     size: 20,
+//     message: `本文は20文字以上入力してください`
+//   }
+// }
+
+
+// setErrorEvent(titleConfig)
+// setErrorEvent(textAreaConfig)
+
+
+
+// function setErrorEvent(obj) {
+//   const $domElement = document.getElementsByName(obj.name)[0]
+
+//   $domElement.addEventListener('keyup', function() {
+//     const textSize = $domElement.value.length;
+//     const btn = document.getElementsByName('btn')[0]
+
+//     let messages = [];  
+  
+//     if (textSize < obj.min.size) {
+//       message.push(obj.min.message)
+//     } else if(textSize > obj.max.size) {
+//       message = obj.max.message
+//       btn.disabled = 'true'
+//     } else {
+//       message = ''
+//       btn.disabled = 'false'
+//     }
+
+//     btn.disabled = 'true'
+//     setErrorMsg(message)
+//   })
+// }
+
+// function setErrorMsg(message) {
+//   const ul = document.getElementsByClassName('error-msg')[0]
+//   const textLi = document.getElementsByClassName('text')[0]
+//   if(textLi == null) {
+//     const li = document.createElement('li');
+//     li.textContent = message;
+//     li.className='text';  
+//     ul.appendChild(li);
+//   } else if(textLi.textContent !== message) {
+//     textLi.textContent = message
+//   }
+// } 
+
+
+// $title.addEventListener('keyup', function() {
+//   const textSize = $title.value.length
+//   const textAreaSize = $textArea.value.length
+//   let message = ''
+
+//   console.log(textSize < 3)
+
+//   if (textAreaSize < 10) {
+//     message = '本文は 10文字以上入力してください'
+//   } else if(textAreaSize > 20) {
+//     message = '本文は20文字以内にしてください'
+//   } else {
+//     message = ''
+//   }
+//   $errorMsg.textContent = message;
+  
+// })
+
+
